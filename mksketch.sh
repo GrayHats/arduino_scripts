4a77e8b74fa1d6d84c609182d8c66436b9cd93cf
allanozzolo <allanon@alaimo.org>
2011-07-12 18:13:04 +0200

    modified:   mksketch.sh modified:   utility/Makefile

--- mksketch.sh
+++ mksketch.sh
@@ -2,13 +2,11 @@
 if [ -n "$1" ]
 then
 	DIR="$1"
-	echo "Creo la cartella"
 	mkdir -p ./sketch/${DIR}
-	echo "Copio i file"
 	cp ./utility/BareMinimum.pde ./sketch/${DIR}/${DIR}.pde
 	cp ./utility/Makefile ./sketch/${DIR}/
-	echo 'fine'
+	echo 'done'
 else
-	echo "Occorre specificare un nome per lo sketch"
+	echo "Usage:\n  ./mksketch.sh nomeproject"
 	exit
 fi
